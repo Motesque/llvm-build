d8556b351699aab10f5f168e319c01bfd1682f5d
Tobias Lang <tobias@motesque.com>
2019-11-05 23:51:57 -0500

    wip

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -26,7 +26,7 @@ echo "Building LLVM for architecture '$ARCH'"
 docker build -t llvm-build-$ARCH --build-arg BALENA_ARCH=$ARCH --build-arg LLVM_ARCH=$LLVM_ARCH .
 
 echo "Copying artifact..."
-if [[ $(uname) == "Darwin" ]]; thens
+if [[ $(uname) == "Darwin" ]]; then
     docker run --rm -v $WORKSPACE/artifacts:/output llvm-build-$ARCH /bin/bash  -c "cp /llvm-*.tar.gz /output/"
 else
     docker run --rm -v /usr/bin/qemu-arm-static:/usr/bin/qemu-arm-static -v $WORKSPACE/artifacts:/output llvm-build-$ARCH /bin/bash  -c "cp /llvm-*.tar.gz /output/"
